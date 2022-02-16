89bf0cdad05205ce69f425ad321db2f23e951e61
moudroff <moudroff>
2022-02-16 13:00:26 +0300

    MVC Second

---
+++ MVCFirst/MVCFirst/Controllers/SecondController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCFirst.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCSecond/MVCSecond/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVCSecond.Models;
+
+namespace MVCSecond.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
