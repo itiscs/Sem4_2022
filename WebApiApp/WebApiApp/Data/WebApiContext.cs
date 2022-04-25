289b6e7fb1a1c820da74ec8edca0913cb54317c5
moudroff <moudroff>
2022-04-25 11:21:38 +0300

    Web Api App

---
+++ WebApiApp/WebApiApp/Data/WebApiContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiApp.Models;
+
+namespace WebApiApp.Data
+{
+    public class WebApiContext : DbContext
+    {
+        public WebApiContext (DbContextOptions<WebApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiApp.Models.Product> Product { get; set; }
+    }
+}
